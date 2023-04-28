5b9557c7b8f87e1e44a873df6da918bb7e36eea3
I-Sora0203 <116257944+I-Sora0203@users.noreply.github.com>
2023-04-28 15:38:50 +0900

    ピースの色を変更
    青を黒に変更．
    他を明るい色に変更．

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+    'ivory', 'wheat', 'black', 'gold', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
